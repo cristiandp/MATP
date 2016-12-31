c643d89c80346d263f78a8eaa46d406aeee68d24
jessicamcav13 <jessicamcav13@gmail.com>
2016-12-31 14:17:37 +0100

    PUSH JESSICA

--- app/src/main/java/com/example/cristian/mamaandroidthermalpos/FragmentAbrirCaja.java
+++ app/src/main/java/com/example/cristian/mamaandroidthermalpos/FragmentAbrirCaja.java
@@ -5,6 +5,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 /**
  * Created by jessi on 30/12/2016.
@@ -13,11 +17,18 @@ import android.view.ViewGroup;
 public class FragmentAbrirCaja extends Fragment {
 
     View view;
+    TextView txtFechayHora;
     @Override
     public View onCreateView(final LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_abrir_caja, container, false);
+
+        txtFechayHora = (TextView) view.findViewById(R.id.txtFechayHora);
+
+        String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+
+        txtFechayHora.setText(currentDateTimeString);
 
         return view;
 
