73eac730cadbc2677a4082c7a4c8bfc5e6d9da65
cristiandp <cristiandp93@gmail.com>
2017-01-02 20:48:48 +0100

    Arreglados Crashes en cerrar Caja

--- app/src/main/java/com/example/cristian/mamaandroidthermalpos/FragmentCierreCaja.java
+++ app/src/main/java/com/example/cristian/mamaandroidthermalpos/FragmentCierreCaja.java
@@ -15,13 +15,15 @@ import android.widget.Toast;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Locale;
 
 /**
  * Created by jessi on 30/12/2016.
  */
 
-public class FragmentCierreCaja extends Fragment {
+public class FragmentCierreCaja extends Fragment{
 
     View view;
     EditText txtFechaActual;
@@ -29,11 +31,9 @@ public class FragmentCierreCaja extends Fragment {
     LinearLayout lyByM;
     Button btnByM;
 
-    EditText edTextCant001, edTextCant002, edTextCant005, edTextCant010, edTextCant020, edTextCant050,
-            edTextCant1, edTextCant2, edTextCant5, edTextCant10, edTextCant20,
-            edTextCant50, edTextCant100, edTextCant200, edTextCant500;
-
     EditText edTextSaldoFinal;
+
+    ArrayList<EditText> saldo = new ArrayList<>();
 
     boolean activado = false;
 
@@ -71,310 +71,42 @@ public class FragmentCierreCaja extends Fragment {
                 activarByM();
             }
         });
-
-        edTextCant001 = (EditText) view.findViewById(R.id.edTxtCant001);
-        edTextCant002 = (EditText) view.findViewById(R.id.edTxtCant002);
-        edTextCant005 = (EditText) view.findViewById(R.id.edTxtCant005);
-        edTextCant010 = (EditText) view.findViewById(R.id.edTxtCant010);
-        edTextCant020 = (EditText) view.findViewById(R.id.edTxtCant020);
-        edTextCant050 = (EditText) view.findViewById(R.id.edTxtCant050);
-        edTextCant1 = (EditText) view.findViewById(R.id.edTxtCant1);
-        edTextCant2 = (EditText) view.findViewById(R.id.edTxtCant2);
-        edTextCant5 = (EditText) view.findViewById(R.id.edTxtCant5);
-        edTextCant10 = (EditText) view.findViewById(R.id.edTxtCant10);
-        edTextCant20 = (EditText) view.findViewById(R.id.edTxtCant20);
-        edTextCant50 = (EditText) view.findViewById(R.id.edTxtCant50);
-        edTextCant100 = (EditText) view.findViewById(R.id.edTxtCant100);
-        edTextCant200 = (EditText) view.findViewById(R.id.edTxtCant200);
-        edTextCant500 = (EditText) view.findViewById(R.id.edTxtCant500);
+        saldo.add((EditText) view.findViewById(R.id.edTxtCant001));
+        saldo.add((EditText) view.findViewById(R.id.edTxtCant002));
+        saldo.add((EditText) view.findViewById(R.id.edTxtCant005));
+        saldo.add((EditText) view.findViewById(R.id.edTxtCant010));
+        saldo.add((EditText) view.findViewById(R.id.edTxtCant020));
+        saldo.add((EditText) view.findViewById(R.id.edTxtCant050));
+        saldo.add((EditText) view.findViewById(R.id.edTxtCant1));
+        saldo.add((EditText) view.findViewById(R.id.edTxtCant2));
+        saldo.add((EditText) view.findViewById(R.id.edTxtCant5));
+        saldo.add((EditText) view.findViewById(R.id.edTxtCant10));
+        saldo.add((EditText) view.findViewById(R.id.edTxtCant20));
+        saldo.add((EditText) view.findViewById(R.id.edTxtCant50));
+        saldo.add((EditText) view.findViewById(R.id.edTxtCant100));
+        saldo.add((EditText) view.findViewById(R.id.edTxtCant200));
+        saldo.add((EditText) view.findViewById(R.id.edTxtCant500));
 
         edTextSaldoFinal = (EditText) view.findViewById(R.id.edTxtsaldoFinal);
 
-        edTextCant001.addTextChangedListener(new TextWatcher() {
-
-            @Override
-            public void afterTextChanged(Editable s) {
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start,
-                                          int count, int after) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start,
-                                      int before, int count) {
-
-                edTextSaldoFinal.setText(String.valueOf(calcularSaldoFinal(edTextCant001)));
-            }
-        });
-
-        edTextCant002.addTextChangedListener(new TextWatcher() {
-
-            @Override
-            public void afterTextChanged(Editable s) {
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start,
-                                          int count, int after) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start,
-                                      int before, int count) {
-
-                edTextSaldoFinal.setText(String.valueOf(calcularSaldoFinal(edTextCant002)));
-            }
-        });
-
-        edTextCant005.addTextChangedListener(new TextWatcher() {
-
-            @Override
-            public void afterTextChanged(Editable s) {
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start,
-                                          int count, int after) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start,
-                                      int before, int count) {
-
-                edTextSaldoFinal.setText(String.valueOf(calcularSaldoFinal(edTextCant005)));
-            }
-        });
-
-        edTextCant010.addTextChangedListener(new TextWatcher() {
-
-            @Override
-            public void afterTextChanged(Editable s) {
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start,
-                                          int count, int after) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start,
-                                      int before, int count) {
-
-                edTextSaldoFinal.setText(String.valueOf(calcularSaldoFinal(edTextCant010)));
-            }
-        });
-
-        edTextCant020.addTextChangedListener(new TextWatcher() {
-
-            @Override
-            public void afterTextChanged(Editable s) {
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start,
-                                          int count, int after) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start,
-                                      int before, int count) {
-
-                edTextSaldoFinal.setText(String.valueOf(calcularSaldoFinal(edTextCant020)));
-            }
-        });
-
-        edTextCant050.addTextChangedListener(new TextWatcher() {
-
-            @Override
-            public void afterTextChanged(Editable s) {
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start,
-                                          int count, int after) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start,
-                                      int before, int count) {
-
-                edTextSaldoFinal.setText(String.valueOf(calcularSaldoFinal(edTextCant050)));
-            }
-        });
-
-        edTextCant1.addTextChangedListener(new TextWatcher() {
-
-            @Override
-            public void afterTextChanged(Editable s) {
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start,
-                                          int count, int after) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start,
-                                      int before, int count) {
-
-                edTextSaldoFinal.setText(String.valueOf(calcularSaldoFinal(edTextCant1)));
-
-            }
-        });
-
-        edTextCant2.addTextChangedListener(new TextWatcher() {
-
-            @Override
-            public void afterTextChanged(Editable s) {
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start,
-                                          int count, int after) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start,
-                                      int before, int count) {
-
-                edTextSaldoFinal.setText(String.valueOf(calcularSaldoFinal(edTextCant2)));
-            }
-        });
-
-        edTextCant5.addTextChangedListener(new TextWatcher() {
-
-            @Override
-            public void afterTextChanged(Editable s) {
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start,
-                                          int count, int after) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start,
-                                      int before, int count) {
-
-                edTextSaldoFinal.setText(String.valueOf(calcularSaldoFinal(edTextCant5)));
-            }
-        });
-
-        edTextCant10.addTextChangedListener(new TextWatcher() {
-
-            @Override
-            public void afterTextChanged(Editable s) {
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start,
-                                          int count, int after) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start,
-                                      int before, int count) {
-
-                edTextSaldoFinal.setText(String.valueOf(calcularSaldoFinal(edTextCant10)));
-            }
-        });
-
-        edTextCant20.addTextChangedListener(new TextWatcher() {
-
-            @Override
-            public void afterTextChanged(Editable s) {
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start,
-                                          int count, int after) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start,
-                                      int before, int count) {
-
-                edTextSaldoFinal.setText(String.valueOf(calcularSaldoFinal(edTextCant20)));
-            }
-        });
-
-        edTextCant50.addTextChangedListener(new TextWatcher() {
-
-            @Override
-            public void afterTextChanged(Editable s) {
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start,
-                                          int count, int after) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start,
-                                      int before, int count) {
-
-                edTextSaldoFinal.setText(String.valueOf(calcularSaldoFinal(edTextCant50)));
-            }
-        });
-
-        edTextCant100.addTextChangedListener(new TextWatcher() {
-
-            @Override
-            public void afterTextChanged(Editable s) {
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start,
-                                          int count, int after) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start,
-                                      int before, int count) {
-
-                edTextSaldoFinal.setText(String.valueOf(calcularSaldoFinal(edTextCant100)));
-            }
-        });
-
-        edTextCant200.addTextChangedListener(new TextWatcher() {
-
-            @Override
-            public void afterTextChanged(Editable s) {
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start,
-                                          int count, int after) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start,
-                                      int before, int count) {
-
-                edTextSaldoFinal.setText(String.valueOf(calcularSaldoFinal(edTextCant200)));
-            }
-        });
-
-        edTextCant500.addTextChangedListener(new TextWatcher() {
-
-            @Override
-            public void afterTextChanged(Editable s) {
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start,
-                                          int count, int after) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start,
-                                      int before, int count) {
-
-                edTextSaldoFinal.setText(String.valueOf(calcularSaldoFinal(edTextCant500)));
-            }
-        });
+        for(int i = 0 ; i < saldo.size();i++){
+            saldo.get(i).addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                }
+
+                @Override
+                public void afterTextChanged(Editable editable) {
+                    edTextSaldoFinal.setText(calcularSaldoFinal());
+                }
+            });
+        }
 
 
         return view;
@@ -402,149 +134,68 @@ public class FragmentCierreCaja extends Fragment {
 
     }
 
-    public float calcularSaldoFinal(EditText edTxt) {
+    public String calcularSaldoFinal() {
+        total = 0f;
 
-        int cant = 0;
+        for (int i = 0; i < saldo.size();i++){
+            String mon;
+            if(!saldo.get(i).getText().toString().equals("")){
+                mon = saldo.get(i).getText().toString();
+            }else{
+                mon = "0";
+            }
 
-        switch (edTxt.getId()) {
-
-            case R.id.edTxtCant001:
-
-                if(edTxt.getText().toString().equals("")){
-                    total = total - ((cant) * 0.01f);
-
-                    Toast.makeText(getContext(),cant, Toast.LENGTH_SHORT).show();
-                }else {
-
-                    cant = Integer.parseInt(edTextCant001.getText().toString());
-
-                    total = total + (cant) * 0.01f;
-                }
-
-                break;
-
-            case R.id.edTxtCant002:
-
-                if(edTxt.getText().toString().equals("")){
-                    total = total - (cant) * 0.02f;
-
-                    Toast.makeText(getContext(),total.toString(), Toast.LENGTH_SHORT).show();
-                }else {
-
-                    cant = Integer.parseInt(edTextCant002.getText().toString());
-
-                    total = total + (cant) * 0.02f;
-
+            switch (i){
+                case 0:
+                    total += Float.parseFloat(mon);
                     break;
-                }
-
-            case R.id.edTxtCant005:
-
-                cant = Integer.parseInt(edTextCant005.getText().toString());
-
-                total = total + (cant) * 0.05f;
-
-                break;
-
-            case R.id.edTxtCant010:
-
-                cant = Integer.parseInt(edTextCant010.getText().toString());
-
-                total = total + (cant) * 0.10f;
-
-                break;
-
-            case R.id.edTxtCant020:
-
-                cant = Integer.parseInt(edTextCant010.getText().toString());
-
-                total = total + (cant) * 0.20f;
-
-                break;
-
-            case R.id.edTxtCant050:
-
-                cant = Integer.parseInt(edTextCant010.getText().toString());
-
-                total = total + (cant) * 0.50f;
-
-                break;
-
-            case R.id.edTxtCant1:
-
-                cant = Integer.parseInt(edTextCant010.getText().toString());
-
-                total = total + (cant) * 1f;
-
-                break;
-
-            case R.id.edTxtCant2:
-
-                cant = Integer.parseInt(edTextCant010.getText().toString());
-
-                total = total + (cant) * 2f;
-
-                break;
-
-            case R.id.edTxtCant5:
-
-                cant = Integer.parseInt(edTextCant010.getText().toString());
-
-                total = total + (cant) * 5f;
-
-                break;
-
-            case R.id.edTxtCant10:
-
-                cant = Integer.parseInt(edTextCant010.getText().toString());
-
-                total = total + (cant) * 10f;
-
-                break;
-
-            case R.id.edTxtCant20:
-
-                cant = Integer.parseInt(edTextCant010.getText().toString());
-
-                total = total + (cant) * 20f;
-
-                break;
-
-            case R.id.edTxtCant50:
-
-                cant = Integer.parseInt(edTextCant010.getText().toString());
-
-                total = total + (cant) * 50f;
-
-                break;
-
-            case R.id.edTxtCant100:
-
-                cant = Integer.parseInt(edTextCant010.getText().toString());
-
-                total = total + (cant) * 100f;
-
-                break;
-
-            case R.id.edTxtCant200:
-
-                cant = Integer.parseInt(edTextCant010.getText().toString());
-
-                total = total + (cant) * 200f;
-
-                break;
-
-            case R.id.edTxtCant500:
-
-                cant = Integer.parseInt(edTextCant010.getText().toString());
-
-                total = total + (cant) * 500f;
-
-                break;
+                case 1:
+                    total += Float.parseFloat(mon)*2;
+                    break;
+                case 2:
+                    total += Float.parseFloat(mon)*5;
+                    break;
+                case 3:
+                    total += Float.parseFloat(mon)*10;
+                    break;
+                case 4:
+                    total += Float.parseFloat(mon)*20;
+                    break;
+                case 5:
+                    total += Float.parseFloat(mon)*50;
+                    break;
+                case 6:
+                    total += Float.parseFloat(mon)*100;
+                    break;
+                case 7:
+                    total += Float.parseFloat(mon)*200;
+                    break;
+                case 8:
+                    total += Float.parseFloat(mon)*500;
+                    break;
+                case 9:
+                    total += Float.parseFloat(mon)*1000;
+                    break;
+                case 10:
+                    total += Float.parseFloat(mon)*2000;
+                    break;
+                case 11:
+                    total += Float.parseFloat(mon)*5000;
+                    break;
+                case 12:
+                    total += Float.parseFloat(mon)*10000;
+                    break;
+                case 13:
+                    total += Float.parseFloat(mon)*20000;
+                    break;
+                case 14:
+                    total += Float.parseFloat(mon)*50000;
+                    break;
+            }
 
         }
-        return total;
 
+        return String.format(Locale.getDefault(),"%.2f",total / 100);
     }
 }
 
